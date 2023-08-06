281a5c3d4e8916b4f914761156fae050c6ded6e5
Sergen1993 <14488@coderacademy.edu.au>
2023-08-06 18:48:47 +1000

    Installed; mongoose; set up Entry schema and model

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const categories = ['Food', 'Gaming', 'Coding', 'Other']
 
 const entries = [
-    { category: "Food", content: "Pizza is yummy!" },
-    { category: "Coding", content: "Coding is fun!" },
-    { category: "Gaming", content: "Skyrim is for the Nords!" },
+  { category: "Food", content: "Pizza is yummy!" },
+  { category: "Coding", content: "Coding is fun!" },
+  { category: "Gaming", content: "Skyrim is for the Nords!" },
 ]
+
+mongoose.connect('mongodb+srv://sergentasdivar93:tookiee1@cluster0.01sz2ak.mongodb.net/journal?retryWrites=true&w=majority')
+  .then(m => console.log(m.connection.readyState === 1 ? 'Mongoose connected!' : 'Mongoose failed to connect'))
+  .catch(err => console.error(err))
+
+const entriesSchema = new mongoose.Schema({
+  category: { type: String, required: true },
+  content: { type: String, required: true }
+})
+
+const EntryModel = mongoose.model('Entry', entriesSchema)
+
 
 const app = express()
 const port = 4001
 
-app.use (express.json())
+app.use(express.json())
 
 app.get('/', (request, response) => response.send({ info: 'Journal API!' }))
 
@@ -20,22 +33,22 @@ app.get('/categories', (req, res) => res.send(categories))
 app.get('/entries', (req, res) => res.send(entries))
 
 app.get('/entries/:id', (req, res) => {
-    const entry = entries[req.params.id]
-    if (entry) {
-        res.send(entry)
-    }   else {
-        res.status(404).send({ error: 'Entry not found' })
-    }
+  const entry = entries[req.params.id]
+  if (entry) {
+    res.send(entry)
+  } else {
+    res.status(404).send({ error: 'Entry not found' })
+  }
 })
 
-app.post('/entries', (req, res) => {
-    // 1. Retreive the data from the request (req)
-    console.log(req.body)
-    // TODO: Validate it
-    // 3. Push the new entry to the entries array
-    entries.push(req.body)
-    // 4. Send the new entry with 201 status
-    res.status(201).send(req.body)
+app.post('/entries', async (req, res) => {
+  try {
+    const insertedEntry = await EntryModel.create(req.body)
+    res.status(201).send(insertedEntry)
+  }
+  catch (err) {
+    res.status(500).send({ error: err.message })
+  }
 })
 
 app.listen(port)
